8afb05cc83ff3298400ed6843e78195921a46597
Fanny Prezeau <fanny.prezeau@etu.univ-poitiers.fr>
2024-09-30 18:14:37 +0200

    Create NumberOfCharacters component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { CharactersList } from './components/CharactersList'
+import { NumberOfCharacters } from './components/NumberOfCharacters'
 
 import characters from './data/characters.json'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <h1 id='title'>Marvel Characters</h1>
       <CharactersList characters={characters}/>
+      <NumberOfCharacters characters={characters}/>
     </>
   )
 }
