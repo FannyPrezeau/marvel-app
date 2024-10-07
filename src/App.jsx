6230489450291744cbf5e795a67f7ee3f83acbb2
Fanny Prezeau <fanny.prezeau@etu.univ-poitiers.fr>
2024-10-07 18:15:40 +0200

    enfin réussi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
-import { CharactersList } from './components/CharactersList'
-import { NumberOfCharacters } from './components/NumberOfCharacters'
 
-import characters from './data/characters.json'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './routes'
+
+// Create a router that uses the client side history strategy for
+const router = createBrowserRouter(routes)
 
 function App() {
   return (
-    <>
-      <h1 id='title'>Marvel Characters</h1>
-      <CharactersList characters={characters}/>
-      <NumberOfCharacters characters={characters}/>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
