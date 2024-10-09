a1f64401a2dca549d6c571ca2d2e0bdd941f8116
Fanny Prezeau <fanny.prezeau@etu.univ-poitiers.fr>
2024-10-09 16:54:48 +0200

    Remise à jour

--- src/components/NumberOfCharacters.jsx
+++ src/components/NumberOfCharacters.jsx
@@ -1,15 +1,10 @@
-
-export function NumberOfCharacters({characters}) {
-    let message;
-    if (characters.length === 0) {
-        message = "There is no character";
-    }
-    else{
-        message = "There is " + characters.length + " characters";
-    }
+export function NumberOfCharacters({ characters =[]}) {
+    const count = characters.length;
+  
     return (
-        <div>
-            {message}
-        </div>
+      <div>
+        {count > 0 ? `There is ${count} character${count > 1 ? 's' : ''}` : 'There is no character'}
+      </div>
     );
   }
+  
--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -1,19 +1,22 @@
-import { CharactersList } from '../components/CharactersList';
-import { NumberOfCharacters } from '../components/NumberOfCharacters';
-import characters from '../data/characters.json'
+// CetteCette façon de faire fonctionne, mais elle n'est pas optimale. Elle mélange la récupération des données et l'affichage des composants.
+// react_router nous permet de faire mieux en utilisant des hooks pour récupérer les données avant d'afficher le composant et ainsi de séparer récupération des données et affichage.
+// Adapter le code pour utiliser react_router et les hooks comme dans l'exemple du guide, grâce aux concepts de loader et useLoaderData . On appelera directement la fonction getCharacters dans le loader (pas de fonction fetch ).
 
-const CharactersPage = () => {
-    document.title = "Characters | MarvelApp";
-    
+import { useLoaderData } from "react-router-dom";
+
+export default function CharactersPage() {
+    const characters = useLoaderData(); // Assurez-vous que cette fonction soit importée correctement
+
     return (
-
-      <>
-        <h1 id="title">Marvel Characters</h1>
-        <CharactersList characters={characters}/>
-        <br />
-        <NumberOfCharacters characters={characters} />
-      </>
+        <div>
+            <h1>Liste des personnages</h1>
+            <ul>
+                {characters.map(character => (
+                    <li key={character.id}>
+                        <a href={`/character/${character.id}`}>{character.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-};
-
-export default CharactersPage;
+}
