c6b1f01c42208d93af0abe55c8d02ab51c5c7b2d
Fanny Prezeau <fanny.prezeau@etu.univ-poitiers.fr>
2024-11-05 17:03:57 +0100

    Test Unitaire

---
+++ src/components/NumberOfCharacters.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { NumberOfCharacters } from './NumberOfCharacters';
+
+test('displays "There is no character" when characters array is empty', () => {
+  const { getByText } = render(<NumberOfCharacters characters={[]} />);
+  expect(getByText('There is no character')).toBeInTheDocument();
+});
+
+test('displays "There is 1 character" when characters array has one character', () => {
+  const { getByText } = render(<NumberOfCharacters characters={['Spider-Man']} />);
+  expect(getByText('There is 1 character')).toBeInTheDocument();
+});
+
+test('displays "There are 2 characters" when characters array has multiple characters', () => {
+  const { getByText } = render(<NumberOfCharacters characters={['Spider-Man', 'Iron Man']} />);
+  expect(getByText('There are 2 characters')).toBeInTheDocument();
+});
