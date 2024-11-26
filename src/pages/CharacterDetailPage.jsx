6a6d2606b15a740bac144c119d415d7eb19d22fe
Fanny Prezeau <fanny.prezeau@etu.univ-poitiers.fr>
2024-11-26 16:46:12 +0100

    modification test characterDetailPage

--- src/pages/CharacterDetailPage.jsx
+++ src/pages/CharacterDetailPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-
 import { useLoaderData } from 'react-router';
 import CharacterDetail from '../components/CharacterDetail';
 import D3PieChart from '../components/D3PieChart';
@@ -10,20 +9,26 @@ const CharacterDetailPage = () => {
     const character = useLoaderData();
 
     useEffect(() => {
-        document.title = `${character.name} | Marvel App`;
+        if (character) {
+            document.title = `${character.name} | Marvel App`;
+        }
     }, [character]);
+
+    if (!character) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <>
             <CharacterDetail character={character} />
 
             <h2>Capacities</h2>
-            <div style={{ display: 'flex'}}>
-                <div style={{flex: '50%'}}>
+            <div style={{ display: 'flex' }}>
+                <div style={{ flex: '50%' }}>
                     <h3>Using D3</h3>
                     <D3PieChart data={character.capacities} />
                 </div>
-                <div style={{flex: '50%'}}>
+                <div style={{ flex: '50%' }}>
                     <h3>Using Recharts</h3>
                     <RechartsPieChart data={character.capacities} />
                 </div>
